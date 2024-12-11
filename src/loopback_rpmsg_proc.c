3f144c4cde2518324e25df66a4a40e954bb122ec
Artem Gulyaev <Artem.Gulyaev@de.bosch.com>
2024-12-11 08:41:06 +0000

    [lic] Updated the license header for rpmsg loopback
    * just this.

--- src/loopback_rpmsg_proc.c
+++ src/loopback_rpmsg_proc.c
@@ -1,11 +1,25 @@
+/* Copyright (c) 2023 Robert Bosch GmbH
+* Artem Gulyaev <Artem.Gulyaev@de.bosch.com>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // SPDX-License-Identifier: GPL-2.0
+
+// Brief file overview:
 //
-// The loopback "remote processor" device to test the local rpmsg drivers.
-//
-// Copyright (C) 2023 Robert Bosch GmbH
-//
-// Artem Gulyaev
-//
+// this is the loopback "remote processor" device to test
+// the local rpmsg drivers.
 
 #define pr_fmt(fmt) "%s: line %d: " fmt, __func__, __LINE__
 
